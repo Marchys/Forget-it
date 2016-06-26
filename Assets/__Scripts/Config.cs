984f7b4c6b8aaf3b8789f94d155632948fc1bde4
Marcel Barros Aparici <barros.marcel92@gmail.com>
2016-06-26 21:48:12 +0200

    fix (config): update say dialog box config

--- Assets/__Scripts/Config.cs
+++ Assets/__Scripts/Config.cs
@@ -12,16 +12,16 @@ public static class Config
             {
                 return new[]
                 {
-                        new SayDialogBox("Standad",new Vector2(252, 6), 22, 32),
-                        new SayDialogBox("Detective",new Vector2(252, 63), 22, 32),
-                        new SayDialogBox("Kalzov",new Vector2(252, 6), 22, 32222222)
+                        new SayDialogBox("Standad",new Vector2(55, 586),new Vector2(437, 227), 875, 454),
+                        new SayDialogBox("Detective",new Vector2(93, 57),new Vector2(532, 119), 685, 238),
+                        new SayDialogBox("Kalzov",new Vector2(55, 586),new Vector2(437, 227), 875, 454)
                  };
             }
         }
 
         public static SayDialogBox StandardConfig
         {
-            get { return new SayDialogBox("Standad", new Vector2(252, 6), 22, 32); }
+            get { return new SayDialogBox("Standad", new Vector2(55, 586), new Vector2(437, 227), 875, 454) }
         }
 
         public enum DialogBoxTypes
--- Assets/__Scripts/SayDialogBox.cs
+++ Assets/__Scripts/SayDialogBox.cs
@@ -5,18 +5,22 @@ public class SayDialogBox
 {
 
     private float _width;
-    private float _height;
 
-    public SayDialogBox(string id, Vector2 position, float width, float height)
+    public SayDialogBox(string id, Vector2 panelPosition, Vector2 position, float width, float height)
     {
-        this._height = height;
-        this._width = width;
+        this.Height = height;
+        this.Width = width;
+        this.PanelPosition = panelPosition;
         this.Position = position;
         this.Id = id;
     }
 
+    public Vector2 PanelPosition { get; private set; }
     public Vector2 Position { get; private set; }
 
     public string Id { get; private set; }
+
+    public float Height { get; private set; }
+    public float Width { get; private set; }
 }
 
